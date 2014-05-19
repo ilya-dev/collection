e8aff48a4b9f38ee0d5ba37ac82dfcca89ba5fc9
Ilya S <public.ilya.s@gmail.com>
2014-05-19 17:45:38 +0700

    write it_is_json_serializable

--- tests/Spec/Collection/CollectionSpec.php
+++ tests/Spec/Collection/CollectionSpec.php
@@ -19,4 +19,15 @@ class CollectionSpec extends ObjectBehavior {
         $this->all()->shouldReturn([1, 2, 3, 4, 5]);
     }
 
+    function it_is_json_serializable()
+    {
+        $this->shouldImplement('Collection\Contracts\JsonableContract');
+
+        $this->jsonSerialize()->shouldBeEqualTo($this->all());
+
+        $this->toJson()->shouldReturn('[1,2,3,4,5]');
+
+        $this->toJson(JSON_PRETTY_PRINT)->shouldNotReturn('[1,2,3,4,5]');
+    }
+
 }
