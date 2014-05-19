dac9e778015e97b96569bd51d1ef6be9042e4081
Ilya S <public.ilya.s@gmail.com>
2014-05-19 17:37:29 +0700

    implement JsonableContract

--- source/Collection/Collection.php
+++ source/Collection/Collection.php
@@ -1,6 +1,8 @@
 <?php namespace Collection;
 
-class Collection {
+use Collection\Contracts\JsonableContract;
+
+class Collection implements JsonableContract {
 
     /**
      * The items stored.
@@ -28,6 +30,27 @@ class Collection {
     public function all()
     {
         return $this->items;
+    }
+
+    /**
+     * Convert the object into a serializable structure.
+     *
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        return $this->items;
+    }
+
+    /**
+     * Get the collection as JSON.
+     *
+     * @param int $options
+     * @return string
+     */
+    public function toJson($options = 0)
+    {
+        return json_encode($this->items, $options);
     }
 
 }
